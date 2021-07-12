f87f040fe7fd35732b5bb7c48bbb9b0c2cbaf982
denbo12 <denbiprivate@gmail.com>
2021-07-12 07:12:54 -0700

    Initial commit

---
+++ app/src/main/java/com/denbofa/weeksixrepo/ContactDAO.kt
@@ -0,0 +1,20 @@
+package com.denbofa.weeksixrepo
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface ContactDAO {
+    @Insert
+    fun addContactItem(contactItem: ContactModel)
+
+    @Query("SELECT * from ContactModel")
+    fun getAllContactItems(): LiveData<List<ContactModel>>
+
+    @Delete
+    fun delete(contactItem: ContactModel)
+}
---
+++ app/src/main/java/com/denbofa/weeksixrepo/ContactDatabase.kt
@@ -0,0 +1,13 @@
+package com.denbofa.weeksixrepo
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = arrayOf(ContactModel::class),
+    version = 1
+)
+
+abstract  class ContactDatabase: RoomDatabase() {
+    abstract fun contactDao(): ContactDAO
+}
